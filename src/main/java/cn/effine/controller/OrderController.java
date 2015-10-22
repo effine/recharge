09d7a5fc42d6ad41eb25c784c05bf0bf1b590e94
effine <verphen@163.com>
2015-10-22 17:24:41 +0800

    add order class

---
+++ src/main/java/cn/effine/controller/OrderController.java
@@ -0,0 +1,21 @@
+/**
+ * @author effine
+ * @Date 2015年10月22日  上午11:14:08
+ * @email verphen#gmail.com
+ * @site http://www.effine.cn
+ */
+
+package cn.effine.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 订单处理
+ */
+@Controller
+@RequestMapping("order")
+public class OrderController {
+
+	
+}
